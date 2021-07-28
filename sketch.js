f997346f7c2c6937f8d9cf9a3f4c12e66ca09e14
Chasefy <88058422+Chasefy@users.noreply.github.com>
2021-07-28 15:33:33 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var weight = [78,120,98,72];
+var totalweight = weight[0]+ weight[1]+ weight[2]+ weight[3];
+console.log(totalweight);
+var avg = totalweight/weight.length;
+console.log(avg);
+  function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
